aca66562765a7fa07246c45108ce975aa4403707
Pranshul Sardana <pranshulsardana2020@gmail.com>
2026-08-29 16:30:11 -0400

    Time: 3 ms (91.04%) | Memory: 21 MB (15.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,20 +5,33 @@ class Solution:
         """
         
         rows, cols = len(matrix), len(matrix[0])
-        r_set, c_set = set(), set()
+        rowZero = False
         
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c] == 0:
-                    r_set.add(r)
-                    c_set.add(c)
+                    matrix[0][c] = 0 # Will be used to set column to 0
 
-        for r in r_set:
-            for c in range(cols):
-                matrix[r][c] = 0
+                    #Make first element of each row 0
+                    if r == 0:
+                        rowZero = True
+                    else:
+                        matrix[r][0] = 0
 
-        for c in c_set:
+        #Setting 0 for everything except the 0th row and column
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+
+        #Check first colums
+        if matrix[0][0] == 0:
             for r in range(rows):
-                matrix[r][c] = 0
+                matrix[r][0] = 0
+
+        #Check first row
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c] = 0
 
         return matrix
