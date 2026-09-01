d4b7f24657549e6d14f0fee4a8ba8aee8e959bad
Pranshul Sardana <pranshulsardana2020@gmail.com>
2026-09-01 13:28:14 -0400

    Time: 12 ms (5.16%) | Memory: 19.9 MB (67.37%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        rows, cols = len(mat), len(mat[0])
+
+        if rows*cols != r*c:
+            return mat
+
+        out_matrix = [[0]*c for _ in range(r)]
+
+        for r_item in range(rows):
+            for c_item in range(cols):
+                index = r_item*cols+c_item
+                print(f"{r_item = }, {c_item = }, {index//c = }, {index%c = }")
+                out_matrix[index//c][index%c] = mat[r_item][c_item]
+
+        return out_matrix
